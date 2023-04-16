ba3f50f0f8d88a0251aed14a794aac8c9020369f
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2023-04-16 20:54:22 +0530

    updated

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -27,7 +27,7 @@ const TodosList = ({todos,setTodos,setEditTodo}) => {
     <div>
         {todos.map((todo)=>(
             <li className='list-item' key={todo.id}>
-                <input type="text" value={todo.title} className={`list ${todo.completed ? "completed":""}`} onChange={(event)=> event.preventDefault()} />
+                <input type="text" value={todo.title} className={`list ${todo.completed ? "complete" : "" }`} onChange={(event)=> event.preventDefault()} />
             <div>
                 <button className='button-complete task-button' onClick={()=> handleComplete(todo)} >
                     <i className='fa fa-check-circle'></i>
